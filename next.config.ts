35b968a60a93cd21824ffe889b5518ff31d0619c
tuananh <letuananh2663@gmail.com>
2025-11-19 12:01:11 +0700

    Added HTTP and HTTPS configurations for google.com in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'google.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'google.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
